44a20294505427fec947dc6fc48ce11c5b6d699c
Dnr0v3c <korolshutovtop@gmail.com>
2025-03-03 19:35:03 +0300

    Services3

--- src/main/java/com/findit/FindIt/service/user/UserService.java
+++ src/main/java/com/findit/FindIt/service/user/UserService.java
@@ -1,7 +1,9 @@
 package com.findit.FindIt.service.user;
 
+import com.findit.FindIt.entity.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
--- src/main/java/com/findit/FindIt/service/user/UserServiceImpl.java
+++ src/main/java/com/findit/FindIt/service/user/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.findit.FindIt.service.user;
 
+import com.findit.FindIt.entity.User;
 import com.findit.FindIt.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,7 +17,8 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService{
 
 
-    private final UserRepository repository;
+
+    private UserRepository repository;
 
 
     @Override
